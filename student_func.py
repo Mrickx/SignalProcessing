2416c91dff1b8465065f3548b857357732e9ee36
Mrickx <aymericdevolder@hotmail.com>
2024-11-27 16:16:28 +0100

    update

--- student_func.py
+++ student_func.py
@@ -5,10 +5,12 @@
 # ## 1 Offline system
 # ### 1.1 Data generation and dataset
 #ici
-#a
+
 # %%
 import numpy as np
 import matplotlib.pyplot as plt
+from Graph import graph
+import os 
 
 def create_sine_wave(f, A, fs, N):
     
@@ -24,10 +26,14 @@ freq = 20
 amplitude = 4
 
 sin=create_sine_wave(freq,amplitude,fs,N)
+plt.plot(sin)
+graph("Signal sinusoïdale de fréquence 20 Hz","Echantillon","Amplitude")
+plt.show()
 
 # %%
 from glob import glob
 import scipy.io.wavfile as wf
+import audiofile 
 
 def read_wavefile(path):
 
@@ -38,16 +44,20 @@ def read_wavefile(path):
 # call and test your function here #
 LocateClaps = "LocateClaps"
 files = glob(f"{LocateClaps}/*.wav")
+x=len(files)
 # Chemin vers le fichier spécifique
-specific_file = files[0]
+all_signals = []
 
-# Appel de la fonction pour lire ce fichier
-signal, sampling_rate = read_wavefile(specific_file)
-
-# Affichage des résultats
-print(f"Fichier : {specific_file}")
-print(f"Signal : {signal} ")  # Aperçu du signal
-print(f"Taux d'échantillonnage : {sampling_rate}")
+# Lire et traiter chaque fichier
+for file in files:
+    signal, sampling_rate = read_wavefile(file)
+    all_signals.append(signal)  # Ajouter le signal à la liste
+plt.plot(all_signals[0])
+graph({files[0]},"Temps","Amplitude")
+plt.show()
+    
+# Afficher le nombre total de fichiers traités
+print(f"{len(files)} fichiers ont été traités.")
 
 # %% [markdown]
 # ### 1.2 Buffering
